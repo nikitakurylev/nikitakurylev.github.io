5230187135626467bb61bfaba6f345e1c63afb57
Nikita Kurylev <n.kurilev@gmail.com>
2022-11-14 12:50:22 +0300

    feat: implement new page with a form

---
+++ scripts/secret.js
@@ -0,0 +1,19 @@
+function onSubmit(form) {
+    if(form.text.value == "")
+        return;
+    var s = localStorage.getItem('list');
+    if(s == null)
+        s = "";
+    s += "<li>" + form.text.value + "</li>";
+    localStorage.setItem('list', s);
+    document.getElementById("list").innerHTML = s;
+}
+
+function onClear() {
+    localStorage.removeItem('list');
+    document.getElementById("list").innerHTML = "";
+}
+
+window.addEventListener('load', (event) => {
+    document.getElementById("list").innerHTML = localStorage.getItem('list');
+});
