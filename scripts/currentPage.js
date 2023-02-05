92a4814c10ed2b5db339e385874db6e4b6d6848c
Nikita Kurylev <n.kurilev@gmail.com>
2023-02-05 14:59:37 +0300

    feat: improve design

--- scripts/currentPage.js
+++ scripts/currentPage.js
@@ -1,8 +1,6 @@
 (function () {
-    console.log("a")
     timeStart = Date.now()
     window.addEventListener('load', (event) => {
-        document.getElementById("footer").innerHTML += " - page loaded in " + (Date.now() - timeStart) + " ms";
         document.getElementById(document.location.pathname).className = "current"
     });
 })();
